7e92044c2a465c1ca09f20c402127d23c5e6fe10
Enzo <enz.dard@gmail.com>
2020-05-29 08:23:53 +0200

    Ajout de la biblio JavaFX depuis Maven

--- src/main/java/fr/umontpellier/iut/groupe1/Main.java
+++ src/main/java/fr/umontpellier/iut/groupe1/Main.java
@@ -6,7 +6,6 @@ import javafx.application.Application;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
