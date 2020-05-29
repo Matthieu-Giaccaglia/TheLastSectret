94833624812ba55794f70a097a31a8d344c6ac31
Matthieu.Giaccaglia <matthieu.giaccaglia@etu.umontpellier.fr>
2020-05-29 12:11:28 +0200

    Ajout image pour taquin

--- src/main/java/fr/umontpellier/iut/groupe2/taquin/Main.java
+++ src/main/java/fr/umontpellier/iut/groupe2/taquin/Main.java
@@ -1,7 +1,8 @@
 package fr.umontpellier.iut.groupe2.taquin;
 
+import fr.umontpellier.iut.commun.data.LayoutLoader;
+import fr.umontpellier.iut.commun.exceptions.LayoutNotFoundException;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,9 +10,18 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("taquin.fxml"));
-        primaryStage.setTitle("Hello World");
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Escape Game S6");
+
+        Parent root = null;
+        try {
+            root = LayoutLoader.getLayout("groupe2/taquin/sample.fxml");
+        } catch (LayoutNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        assert root != null;
         primaryStage.setScene(new Scene(root, 900, 1080));
         primaryStage.show();
     }
