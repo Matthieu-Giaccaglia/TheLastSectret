6524014c8bd05eb7bed64a71cb999b1fe995c794
walter-deneuville <walter.deneuville@etu.umontpellier.fr>
2020-06-02 16:21:03 +0200

    LightsOut fxml missing graphic switch

--- src/main/java/fr/umontpellier/iut/groupe2/lightsout/LightsOut.java
+++ src/main/java/fr/umontpellier/iut/groupe2/lightsout/LightsOut.java
@@ -1,94 +1,94 @@
 package fr.umontpellier.iut.groupe2.lightsout;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class LightsOut {
     private int[][] tableau;
-    private static HashMap<int[], Integer> Tab_ID;
+    private static HashMap<String, Integer> Tab_ID;
 
     static {
         Tab_ID = new HashMap<>();
 
-        Tab_ID.put(new int[]{0,0},0);
-        Tab_ID.put(new int[]{0,1},1);
-        Tab_ID.put(new int[]{0,2},2);
-        Tab_ID.put(new int[]{0,3},3);
+        Tab_ID.put(Arrays.toString(new int[]{0, 0}),0);
+        Tab_ID.put(Arrays.toString(new int[]{0,1}),1);
+        Tab_ID.put(Arrays.toString(new int[]{0,2}),2);
+        Tab_ID.put(Arrays.toString(new int[]{0,3}),3);
 
-        Tab_ID.put(new int[]{1,0},4);
-        Tab_ID.put(new int[]{1,1},5);
-        Tab_ID.put(new int[]{1,2},6);
-        Tab_ID.put(new int[]{1,3},7);
+        Tab_ID.put(Arrays.toString(new int[]{1,0}),4);
+        Tab_ID.put(Arrays.toString(new int[]{1,1}),5);
+        Tab_ID.put(Arrays.toString(new int[]{1,2}),6);
+        Tab_ID.put(Arrays.toString(new int[]{1,3}),7);
 
-        Tab_ID.put(new int[]{2,0},8);
-        Tab_ID.put(new int[]{2,1},9);
-        Tab_ID.put(new int[]{2,2},10);
-        Tab_ID.put(new int[]{2,3},11);
+        Tab_ID.put(Arrays.toString(new int[]{2,0}),8);
+        Tab_ID.put(Arrays.toString(new int[]{2,1}),9);
+        Tab_ID.put(Arrays.toString(new int[]{2,2}),10);
+        Tab_ID.put(Arrays.toString(new int[]{2,3}),11);
 
-        Tab_ID.put(new int[]{3,0},12);
-        Tab_ID.put(new int[]{3,1},13);
-        Tab_ID.put(new int[]{3,2},14);
-        Tab_ID.put(new int[]{3,3},15);
+        Tab_ID.put(Arrays.toString(new int[]{3,0}),12);
+        Tab_ID.put(Arrays.toString(new int[]{3,1}),13);
+        Tab_ID.put(Arrays.toString(new int[]{3,2}),14);
+        Tab_ID.put(Arrays.toString(new int[]{3,3}),15);
     }
 
-    public static int getID(int[] pos) {
-        return Tab_ID.get(pos);
+    public static int getID(int i, int j) {
+        return Tab_ID.get(Arrays.toString(new int[]{i, j}));
     }
 
     public LightsOut(int[][] tableau) {
         this.tableau = tableau;
     }
 
-    public void onClick(int[] position){
+    public void onClick(int pos_i, int pos_j){
         int max_i = tableau.length - 1;
         int max_j = tableau[0].length - 1;
-
-        if (position[0] == 0){//barre haut
-            if(position[1] == 0){//gauche
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]+1,position[1]);
-                lightSwitch(position[0],position[1]+1);
-            }else if(position[1] == max_j){//droite
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]+1,position[1]);
-                lightSwitch(position[0],position[1]-1);
+        if (pos_i == 0){//barre haut
+            if(pos_j == 0){//gauche
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i+1,pos_j);
+                lightSwitch(pos_i,pos_j+1);
+            }else if(pos_j == max_j){//droite
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i+1,pos_j);
+                lightSwitch(pos_i,pos_j-1);
             } else{//milieu
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0],position[1]+1);
-                lightSwitch(position[0]+1,position[1]);
-                lightSwitch(position[0],position[1]-1);
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i,pos_j+1);
+                lightSwitch(pos_i+1,pos_j);
+                lightSwitch(pos_i,pos_j-1);
             }
-        }else if(position[0] == max_i){//barre bas
-            if(position[1] == 0){//gauche
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]-1,position[1]);
-                lightSwitch(position[0],position[1]+1);
-            }else if(position[1] == max_j){//droite
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]-1,position[1]);
-                lightSwitch(position[0],position[1]-1);
+        }else if(pos_i == max_i){//barre bas
+            if(pos_j == 0){//gauche
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i-1,pos_j);
+                lightSwitch(pos_i,pos_j+1);
+            }else if(pos_j == max_j){//droite
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i-1,pos_j);
+                lightSwitch(pos_i,pos_j-1);
             } else{//milieu
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0],position[1]+1);
-                lightSwitch(position[0]-1,position[1]);
-                lightSwitch(position[0],position[1]-1);
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i,pos_j+1);
+                lightSwitch(pos_i-1,pos_j);
+                lightSwitch(pos_i,pos_j-1);
             }
         }else{//barres milieu
-            if(position[1] == 0){//gauche
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]-1,position[1]);
-                lightSwitch(position[0]+1,position[1]);
-                lightSwitch(position[0],position[1]+1);
-            }else if(position[1] == max_j){//droite
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]-1,position[1]);
-                lightSwitch(position[0]+1,position[1]);
-                lightSwitch(position[0],position[1]-1);
+            if(pos_j == 0){//gauche
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i-1,pos_j);
+                lightSwitch(pos_i+1,pos_j);
+                lightSwitch(pos_i,pos_j+1);
+            }else if(pos_j == max_j){//droite
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i-1,pos_j);
+                lightSwitch(pos_i+1,pos_j);
+                lightSwitch(pos_i,pos_j-1);
             } else{//milieu
-                lightSwitch(position[0],position[1]);
-                lightSwitch(position[0]-1,position[1]);
-                lightSwitch(position[0]+1,position[1]);
-                lightSwitch(position[0],position[1]-1);
-                lightSwitch(position[0],position[1]+1);
+                lightSwitch(pos_i,pos_j);
+                lightSwitch(pos_i-1,pos_j);
+                lightSwitch(pos_i+1,pos_j);
+                lightSwitch(pos_i,pos_j-1);
+                lightSwitch(pos_i,pos_j+1);
             }
         }
 
@@ -118,18 +118,19 @@ public class LightsOut {
 
     public int[] trouverCoordonne(int id){
         int[] coordonnee = new int[2];
-        int[] position = new int[2];
         for (int i=0; i<tableau.length; i++){
             for(int j=0 ; j<tableau.length; j++){
-                position[0] = i;
-                position[1] = j;
-                if (getID(position) == id){
+                if (getID(i,j) == id){
                     coordonnee[0]=i;
                     coordonnee[1]=j;
                 }
             }
         }
         return coordonnee;
+    }
+
+    public int[][] getTableau() {
+        return tableau;
     }
 
     @Override
--- src/main/java/fr/umontpellier/iut/groupe2/lightsout/LightsOutController.java
+++ src/main/java/fr/umontpellier/iut/groupe2/lightsout/LightsOutController.java
@@ -1,11 +1,14 @@
 package fr.umontpellier.iut.groupe2.lightsout;
 
 import javafx.fxml.FXML;
-import javafx.scene.Node;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class LightsOutController {
     @FXML
@@ -14,53 +17,111 @@ public class LightsOutController {
     private GridPane gridPane;
     @FXML
     private ImageView un, deux, trois, quatre, cinq, six, sept, huit, neuf, dix, onze, douze, treize, quatorze, quinze, seize;
+    @FXML
+    private ImageView[] tab_imgView = {un, deux, trois, quatre, cinq, six, sept, huit, neuf, dix, onze, douze, treize, quatorze, quinze, seize};
 
-    private int[][] tab_lo = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};;
+    private int[][] tab_lo = {{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
     private LightsOut Lout = new LightsOut(tab_lo);
+    private static HashMap<Integer, String> Tab_ID;
 
-    void onMouseClicked(MouseEvent event) {
+    static {
+        Tab_ID = new HashMap<>();
+
+        Tab_ID.put(0,"un");
+        Tab_ID.put(1,"deux");
+        Tab_ID.put(2,"trois");
+        Tab_ID.put(3,"quatre");
+
+        Tab_ID.put(4,"cinq");
+        Tab_ID.put(5,"six");
+        Tab_ID.put(6,"sept");
+        Tab_ID.put(7,"huit");
+
+        Tab_ID.put(8,"neuf");
+        Tab_ID.put(9,"dix");
+        Tab_ID.put(10,"onze");
+        Tab_ID.put(11,"douze");
+
+        Tab_ID.put(12,"treize");
+        Tab_ID.put(13,"quatorze");
+        Tab_ID.put(14,"quinze");
+        Tab_ID.put(15,"seize");
+    }
+
+    public static String getTabString(int num_id) {
+        return Tab_ID.get(num_id);
+    }
+
+    public void light_switch(MouseEvent event) {
 
         if (!Lout.estGagnant()) {
 
             if (event.getSource() == un) {
-                updateScene(1, un);
+                updateScene(0, un);
             } else if (event.getSource() == deux) {
-                updateScene(2, deux);
+                updateScene(1, deux);
             } else if (event.getSource() == trois) {
-                updateScene(3, trois);
+                updateScene(2, trois);
             } else if (event.getSource() == quatre) {
-                updateScene(4, quatre);
+                updateScene(3, quatre);
             } else if (event.getSource() == cinq) {
-                updateScene(5, cinq);
+                updateScene(4, cinq);
             } else if (event.getSource() == six) {
-                updateScene(6, six);
+                updateScene(5, six);
             } else if (event.getSource() == sept) {
-                updateScene(7, sept);
+                updateScene(6, sept);
             } else if (event.getSource() == huit) {
-                updateScene(8, huit);
+                updateScene(7, huit);
             } else if (event.getSource() == neuf) {
-                updateScene(9, neuf);
+                updateScene(8, neuf);
             } else if (event.getSource() == dix) {
-                updateScene(10, dix);
+                updateScene(9, dix);
             } else if (event.getSource() == onze) {
-                updateScene(11, onze);
+                updateScene(10, onze);
             } else if (event.getSource() == douze) {
-                updateScene(12, douze);
+                updateScene(11, douze);
             } else if (event.getSource() == treize) {
-                updateScene(13, treize);
+                updateScene(12, treize);
             } else if (event.getSource() == quatorze) {
-                updateScene(14, quatorze);
+                updateScene(13, quatorze);
             } else if (event.getSource() == quinze) {
-                updateScene(15, quinze);
+                updateScene(14, quinze);
             } else if (event.getSource() == seize) {
-                updateScene(16, seize);
+                updateScene(15, seize);
             }
         }
     }
-    public void updateScene (int i, Node node){
-
-        int [] coordonne = Lout.trouverCoordonne(0);
-        Lout.lightSwitch(coordonne[0],coordonne[1]);
-        GridPane.setConstraints(node, coordonne[1], coordonne[0]);
+    public void updateScene (int i, ImageView img){
+        int [] coordonne = Lout.trouverCoordonne(i);
+        Lout.onClick(coordonne[0],coordonne[1]);
+        System.out.println(Lout.toString());
+        for(int j = 0; j< Lout.getTableau().length; j++) {
+            for (int k=0; k<Lout.getTableau()[0].length; k++){
+                if (Lout.getTableau()[j][k] == 1) {
+                    if(compare(j,k)) {
+                        tab_imgView[j+k].setImage(new Image("https://media-exp1.licdn.com/dms/image/C560BAQHMnA03XDdf3w/company-logo_200_200/0?e=2159024400&v=beta&t=C7KMOtnrJwGrMXmgIk2u1B8a7VRfgxMwXng9cdP9kZk"));
+                    }
+                }
+                else{
+                    if(compare(j,k)) {
+                        tab_imgView[j+k].setImage(new Image("https://i.pinimg.com/236x/c4/b8/22/c4b822d890fe6d90358f5e2f2561cd56.jpg"));
+                    }
+                }
+            }
+        }
+    }
+    public boolean compare(int i_tabid,int j_tabid){
+        for(int i = 0; i<Lout.getTableau().length;i++){
+            for(int j = 0; j<Lout.getTableau()[0].length;j++) {
+                System.out.println(LightsOutController.getTabString(LightsOut.getID(i_tabid, j_tabid)));
+                System.out.println(j);
+                System.out.println(i);
+                System.out.println(tab_imgView[j + i]);
+                if (LightsOutController.getTabString(LightsOut.getID(i_tabid, j_tabid)).equals(tab_imgView[j + i].getId())){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
