75cd1ae5b60525c4b819ea8005a1f748bf800fb1
Arnaud Sahonet <arnaud.sahonet@etu.umontpellier.fr>
2020-06-05 17:53:58 +0200

    commit oui

--- src/main/java/fr/umontpellier/iut/groupe1/enigmesymbole/TrapezeController.java
+++ src/main/java/fr/umontpellier/iut/groupe1/enigmesymbole/TrapezeController.java
@@ -1,9 +1,7 @@
 package fr.umontpellier.iut.groupe1.enigmesymbole;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -15,10 +13,10 @@ public class TrapezeController implements Initializable {
 
     /*------------------- 1er -------------------*/
     @FXML
-    private ImageView flecheDroite0;
+    private ImageView flecheGauche0;
 
     @FXML
-    private ImageView flecheGauche0;
+    private ImageView flecheDroite0;
 
     @FXML
     private ImageView aigle0;
@@ -35,6 +33,18 @@ public class TrapezeController implements Initializable {
     @FXML
     private ImageView background;
 
+    @FXML
+    private ImageView aigleCote0;
+
+    @FXML
+    private ImageView serpentCote0;
+
+    @FXML
+    private ImageView dauphinCote0;
+
+    @FXML
+    private ImageView videCote0;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         background.fitWidthProperty().bind(Main.stage.widthProperty());
@@ -42,36 +52,52 @@ public class TrapezeController implements Initializable {
 
     @FXML
     void handleButtonAction0(MouseEvent actionEvent) {
-        if (actionEvent.getSource() == flecheDroite0 && aigle0.isVisible()) {
+        if (actionEvent.getSource() == flecheGauche0 && aigle0.isVisible()) {
             aigle0.setVisible(false);
             serpent0.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche0 && aigle0.isVisible()){
+            serpentCote0.setVisible(false);
+            videCote0.setVisible(true);
+        }else if (actionEvent.getSource() == flecheDroite0 && aigle0.isVisible()){
             aigle0.setVisible(false);
             dauphin0.setVisible(true);
+            serpentCote0.setVisible(false);
+            aigleCote0.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite0 && serpent0.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche0 && serpent0.isVisible()){
             serpent0.setVisible(false);
             vide0.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche0 && serpent0.isVisible()){
+            videCote0.setVisible(false);
+            dauphinCote0.setVisible(true);
+        }else if (actionEvent.getSource() == flecheDroite0 && serpent0.isVisible()){
             serpent0.setVisible(false);
             aigle0.setVisible(true);
+            videCote0.setVisible(false);
+            serpentCote0.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite0 && vide0.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche0 && vide0.isVisible()){
             vide0.setVisible(false);
             dauphin0.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche0 && vide0.isVisible()){
+            dauphinCote0.setVisible(false);
+            aigleCote0.setVisible(true);
+        }else if (actionEvent.getSource() == flecheDroite0 && vide0.isVisible()){
             vide0.setVisible(false);
             serpent0.setVisible(true);
+            dauphinCote0.setVisible(false);
+            videCote0.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite0 && dauphin0.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche0 && dauphin0.isVisible()){
             dauphin0.setVisible(false);
             aigle0.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche0 && dauphin0.isVisible()){
+            aigleCote0.setVisible(false);
+            serpentCote0.setVisible(true);
+        }else if (actionEvent.getSource() == flecheDroite0 && dauphin0.isVisible()){
             dauphin0.setVisible(false);
             vide0.setVisible(true);
+            aigleCote0.setVisible(false);
+            dauphinCote0.setVisible(true);
         }
 
     }
@@ -80,10 +106,10 @@ public class TrapezeController implements Initializable {
     /*------------------- 2eme -------------------*/
 
     @FXML
-    private ImageView flecheDroite1;
+    private ImageView flecheGauche1;
 
     @FXML
-    private ImageView flecheGauche1;
+    private ImageView flecheDroite1;
 
     @FXML
     private ImageView aigle1;
@@ -99,34 +125,34 @@ public class TrapezeController implements Initializable {
 
     @FXML
     void handleButtonAction1(MouseEvent actionEvent) {
-        if (actionEvent.getSource() == flecheDroite1 && aigle1.isVisible()) {
+        if (actionEvent.getSource() == flecheGauche1 && aigle1.isVisible()) {
             aigle1.setVisible(false);
             serpent1.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche1 && aigle1.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite1 && aigle1.isVisible()){
             aigle1.setVisible(false);
             dauphin1.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite1 && serpent1.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche1 && serpent1.isVisible()){
             serpent1.setVisible(false);
             vide1.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche1 && serpent1.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite1 && serpent1.isVisible()){
             serpent1.setVisible(false);
             aigle1.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite1 && vide1.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche1 && vide1.isVisible()){
             vide1.setVisible(false);
             dauphin1.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche1 && vide1.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite1 && vide1.isVisible()){
             vide1.setVisible(false);
             serpent1.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite1 && dauphin1.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche1 && dauphin1.isVisible()){
             dauphin1.setVisible(false);
             aigle1.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche1 && dauphin1.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite1 && dauphin1.isVisible()){
             dauphin1.setVisible(false);
             vide1.setVisible(true);
         }
@@ -137,10 +163,10 @@ public class TrapezeController implements Initializable {
     /*------------------- 3eme -------------------*/
 
     @FXML
-    private ImageView flecheDroite2;
+    private ImageView flecheGauche2;
 
     @FXML
-    private ImageView flecheGauche2;
+    private ImageView flecheDroite2;
 
     @FXML
     private ImageView aigle2;
@@ -156,34 +182,34 @@ public class TrapezeController implements Initializable {
 
     @FXML
     void handleButtonAction2(MouseEvent actionEvent) {
-        if (actionEvent.getSource() == flecheDroite2 && aigle2.isVisible()) {
+        if (actionEvent.getSource() == flecheGauche2 && aigle2.isVisible()) {
             aigle2.setVisible(false);
             serpent2.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche2 && aigle2.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite2 && aigle2.isVisible()){
             aigle2.setVisible(false);
             dauphin2.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite2 && serpent2.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche2 && serpent2.isVisible()){
             serpent2.setVisible(false);
             vide2.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche2 && serpent2.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite2 && serpent2.isVisible()){
             serpent2.setVisible(false);
             aigle2.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite2 && vide2.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche2 && vide2.isVisible()){
             vide2.setVisible(false);
             dauphin2.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche2 && vide2.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite2 && vide2.isVisible()){
             vide2.setVisible(false);
             serpent2.setVisible(true);
         }
 
-        else if (actionEvent.getSource() == flecheDroite2 && dauphin2.isVisible()){
+        else if (actionEvent.getSource() == flecheGauche2 && dauphin2.isVisible()){
             dauphin2.setVisible(false);
             aigle2.setVisible(true);
-        }else if (actionEvent.getSource() == flecheGauche2 && dauphin2.isVisible()){
+        }else if (actionEvent.getSource() == flecheDroite2 && dauphin2.isVisible()){
             dauphin2.setVisible(false);
             vide2.setVisible(true);
         }
