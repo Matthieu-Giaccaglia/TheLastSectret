399e06cfe31fc86218a4e207aff3f151bd2c633b
Maxime <maxime.poujol01@etu.umontpellier.fr>
2020-06-05 18:01:43 +0200

    Labyrinthe

--- src/main/java/fr/umontpellier/iut/groupe1/controllers/MainController.java
+++ src/main/java/fr/umontpellier/iut/groupe1/controllers/MainController.java
@@ -1,4 +1,24 @@
 package fr.umontpellier.iut.groupe1.controllers;
 
-public class MainController {
+import fr.umontpellier.iut.groupe1.thread.ThreadTimer;
+import javafx.animation.AnimationTimer;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    public Label label_room;
+    public Label timerJeu;
+
+
+    @Override
+    public synchronized void initialize(URL url, ResourceBundle resourceBundle) {
+
+        Thread threadTimer = new Thread(new ThreadTimer(timerJeu));
+        threadTimer.start();
+
+    }
 }
---
+++ src/main/java/fr/umontpellier/iut/groupe1/thread/ThreadTimer.java
@@ -0,0 +1,33 @@
+package fr.umontpellier.iut.groupe1.thread;
+
+
+import javafx.scene.control.Label;
+
+public class ThreadTimer implements Runnable{
+
+    private int secondes = 0;
+    private Label label;
+
+    public ThreadTimer(Label label){
+        this.label = label;
+    }
+
+    @Override
+    public synchronized void run() {
+
+        int minutes = 5;
+        //while (minutes > -1){  }
+        label.setText(minutes + ":" + secondes);
+        System.out.println("ici");
+
+        try {
+            this.wait(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        secondes--;
+
+        label.setText(minutes + ":" + secondes);
+        System.out.println(label.getText());
+    }
+}
