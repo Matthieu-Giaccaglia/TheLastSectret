e8215077b9c430dacfaeb6e97ada01683c9c5a55
Matthieu.Giaccaglia <matthieu.giaccaglia@etu.umontpellier.fr>
2020-06-05 16:07:34 +0200

    Update StepManager Groupe2

--- src/main/java/fr/umontpellier/iut/groupe2/view/StepID.java
+++ src/main/java/fr/umontpellier/iut/groupe2/view/StepID.java
@@ -3,7 +3,8 @@ package fr.umontpellier.iut.groupe2.view;
 public enum StepID {
     START(0),
     TAQUIN(1),
-    LIGHTSOUT(2);
+    LIGHTSOUT(2),
+    INVENTAIRE(3);
 
 
     private final int id;
