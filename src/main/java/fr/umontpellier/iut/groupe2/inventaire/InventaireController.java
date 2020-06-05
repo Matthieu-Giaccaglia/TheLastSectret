2b39d1bb934648e5d64bc8111359116d475b8b03
Matthieu.Giaccaglia <matthieu.giaccaglia@etu.umontpellier.fr>
2020-06-05 11:35:54 +0200

    Update Invenraire

--- src/main/java/fr/umontpellier/iut/groupe2/inventaire/InventaireController.java
+++ src/main/java/fr/umontpellier/iut/groupe2/inventaire/InventaireController.java
@@ -5,6 +5,7 @@ import fr.umontpellier.iut.groupe2.MainSalleGroupe2;
 import fr.umontpellier.iut.groupe2.view.StepManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -22,4 +23,7 @@ public class InventaireController {
     @FXML
     private GridPane inventoryGrid;
 
+    public void updateInventaireGraphique(Node node){
+        inventoryGrid.add(node,0,0);
+    }
 }
