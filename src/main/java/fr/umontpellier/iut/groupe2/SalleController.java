6c15edf66dcc244981184e42ab39d4869ba1dbdc
Mateusz-Birembaut <mateusz.birembaut@etu.umontpellier.fr>
2020-06-08 21:27:11 +0200

    Flo bg que t'es 2

--- src/main/java/fr/umontpellier/iut/groupe2/SalleController.java
+++ src/main/java/fr/umontpellier/iut/groupe2/SalleController.java
@@ -237,12 +237,12 @@ public class SalleController {
 
     private void animationPilierTremblement() {
         TranslateTransition translatePilier = new TranslateTransition(Duration.seconds(0.1),pilierGrand);
-        translatePilier.setByX(-5);
-        translatePilier.setByY(10);
+        translatePilier.setByX(-2);
+        translatePilier.setByY(-2);
 
         TranslateTransition translateGemmeVerte = new TranslateTransition(Duration.seconds(0.1),gemmeTropHaute);
-        translateGemmeVerte.setByX(-5);
-        translateGemmeVerte.setByY(10);
+        translateGemmeVerte.setByX(-2);
+        translateGemmeVerte.setByY(-2);
 
         ParallelTransition parallelPilier = new ParallelTransition(translatePilier, translateGemmeVerte);
         parallelPilier.setCycleCount(2);
