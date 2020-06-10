d02361fd30536652510a222174d3f32712c3419e
Maxime <maxime.poujol01@etu.umontpellier.fr>
2020-06-10 15:12:52 +0200

    menu

--- src/main/java/fr/umontpellier/iut/groupe1/labyrinthe/BackgroundStackPane.java
+++ src/main/java/fr/umontpellier/iut/groupe1/labyrinthe/BackgroundStackPane.java
@@ -2,6 +2,7 @@ package fr.umontpellier.iut.groupe1.labyrinthe;
 
 import fr.umontpellier.iut.groupe1.data.ImageLoader;
 import javafx.scene.Group;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.StackPane;
@@ -17,9 +18,21 @@ public class BackgroundStackPane extends StackPane {
     }
 
     public BackgroundStackPane(Group group, double width, double height){
+
         ImageView image = new ImageView(ImageLoader.getImage("groupe1/wall_15.jpg"));
         image.setFitWidth(width);
         image.setFitHeight(height);
         this.getChildren().addAll(image, group);
+    }
+
+    public BackgroundStackPane(Group group, double widthImageFond, double heightImageFond, double widthBackground, double heightBackground){
+        ImageView fondNoir = new ImageView(ImageLoader.getImage("groupe1/noir2.jpg"));
+        fondNoir.setFitWidth(widthBackground);
+        fondNoir.setFitHeight(heightBackground);
+        fondNoir.setOpacity(0.8);
+        ImageView image = new ImageView(ImageLoader.getImage("groupe1/wall_15.jpg"));
+        image.setFitWidth(widthImageFond);
+        image.setFitHeight(heightImageFond);
+        this.getChildren().addAll(fondNoir, image, group);
     }
 }
