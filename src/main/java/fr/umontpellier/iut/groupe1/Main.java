4445fcdab209b0a4a5efcca4c7b75771fa8f9e4b
enzo-dardaillon <enzo.dardaillon@etu.umontpellier.fr>
2020-06-10 20:00:01 +0200

    Step.onStart

--- src/main/java/fr/umontpellier/iut/groupe1/Main.java
+++ src/main/java/fr/umontpellier/iut/groupe1/Main.java
@@ -6,12 +6,14 @@ import fr.umontpellier.iut.groupe1.data.Layout;
 import fr.umontpellier.iut.groupe1.data.LayoutLoader;
 import fr.umontpellier.iut.groupe1.labyrinthe.BackgroundStackPane;
 import fr.umontpellier.iut.groupe1.menu.MenuPause;
+import fr.umontpellier.iut.groupe1.utils.Callback;
 import fr.umontpellier.iut.groupe1.view.Step;
 import fr.umontpellier.iut.groupe1.view.StepID;
 import fr.umontpellier.iut.groupe1.view.StepManager;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -37,7 +39,13 @@ public class Main extends Application {
             stepManager.addStep(new Step<>(StepID.CAM1, LayoutLoader.getLayout2("groupe1/salles/layout_cam1.fxml")));
             stepManager.addStep(new Step<>(StepID.CAM2, LayoutLoader.getLayout2("groupe1/salles/layout_cam2.fxml")));
             stepManager.addStep(new Step<>(StepID.CAM3, LayoutLoader.getLayout2("groupe1/salles/layout_cam3.fxml")));
-            stepManager.addStep(new Step<>(StepID.CAM4, LayoutLoader.getLayout2("groupe1/layout_trapeze.fxml")));
+
+            Step<Parent> trapeze = new Step<>(StepID.CAM4, LayoutLoader.getLayout2("groupe1/layout_trapeze.fxml"));
+            trapeze.setOnStart(() -> {
+                //todo ajouter son
+            });
+
+            stepManager.addStep(trapeze);
             stepManager.addStep(new Step<>(StepID.CAM5, LayoutLoader.getLayout2("groupe1/salles/layout_cam5.fxml")));
             stepManager.addStep(new Step<>(StepID.CAM6, LayoutLoader.getLayout2("groupe1/salles/layout_cam6.fxml")));
             stepManager.addStep(new Step<>(StepID.CAM7, LayoutLoader.getLayout2("groupe1/salles/layout_cam7.fxml")));
