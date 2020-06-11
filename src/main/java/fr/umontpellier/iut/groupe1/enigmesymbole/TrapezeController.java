fbf76e3279240625316eec174dc6ceade3800d5b
Arnaud Sahonet <arnaud.sahonet@etu.umontpellier.fr>
2020-06-12 01:28:00 +0200

    Controller test

--- src/main/java/fr/umontpellier/iut/groupe1/enigmesymbole/TrapezeController.java
+++ src/main/java/fr/umontpellier/iut/groupe1/enigmesymbole/TrapezeController.java
@@ -50,6 +50,10 @@ public class TrapezeController implements Initializable {
         }
     }
 
+    public void sonPierre(){
+        new MediaPlayer(new Media(Paths.get("src/main/resources/sound/groupe1/sonMoveTrapeze.mp3").toUri().toString())).play();
+    }
+
 
     /*------------------- 1er -------------------*/
     @FXML
@@ -98,11 +102,13 @@ public class TrapezeController implements Initializable {
             serpent0.setVisible(true);
             serpentCote0.setVisible(false);
             videCote0.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite0 && aigle0.isVisible()){
             aigle0.setVisible(false);
             dauphin0.setVisible(true);
             serpentCote0.setVisible(false);
             aigleCote0.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche0 && serpent0.isVisible()){
@@ -110,11 +116,13 @@ public class TrapezeController implements Initializable {
             vide0.setVisible(true);
             videCote0.setVisible(false);
             dauphinCote0.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite0 && serpent0.isVisible()){
             serpent0.setVisible(false);
             aigle0.setVisible(true);
             videCote0.setVisible(false);
             serpentCote0.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche0 && vide0.isVisible()){
@@ -122,11 +130,13 @@ public class TrapezeController implements Initializable {
             dauphin0.setVisible(true);
             dauphinCote0.setVisible(false);
             aigleCote0.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite0 && vide0.isVisible()){
             vide0.setVisible(false);
             serpent0.setVisible(true);
             dauphinCote0.setVisible(false);
             videCote0.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche0 && dauphin0.isVisible()){
@@ -134,11 +144,13 @@ public class TrapezeController implements Initializable {
             aigle0.setVisible(true);
             aigleCote0.setVisible(false);
             serpentCote0.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite0 && dauphin0.isVisible()){
             dauphin0.setVisible(false);
             vide0.setVisible(true);
             aigleCote0.setVisible(false);
             dauphinCote0.setVisible(true);
+            sonPierre();
         }
 
     }
@@ -169,33 +181,41 @@ public class TrapezeController implements Initializable {
         if (actionEvent.getSource() == flecheGauche1 && aigle1.isVisible()) {
             aigle1.setVisible(false);
             serpent1.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite1 && aigle1.isVisible()){
             aigle1.setVisible(false);
             dauphin1.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche1 && serpent1.isVisible()){
             serpent1.setVisible(false);
             vide1.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite1 && serpent1.isVisible()){
             serpent1.setVisible(false);
             aigle1.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche1 && vide1.isVisible()){
             vide1.setVisible(false);
             dauphin1.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite1 && vide1.isVisible()){
             vide1.setVisible(false);
             serpent1.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche1 && dauphin1.isVisible()){
             dauphin1.setVisible(false);
             aigle1.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite1 && dauphin1.isVisible()){
             dauphin1.setVisible(false);
             vide1.setVisible(true);
+            sonPierre();
         }
 
     }
@@ -240,11 +260,13 @@ public class TrapezeController implements Initializable {
             serpent2.setVisible(true);
             dauphinCote2.setVisible(false);
             aigleCote2.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite2 && aigle2.isVisible()){
             aigle2.setVisible(false);
             dauphin2.setVisible(true);
             dauphinCote2.setVisible(false);
             videCote2.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche2 && serpent2.isVisible()){
@@ -252,11 +274,13 @@ public class TrapezeController implements Initializable {
             vide2.setVisible(true);
             aigleCote2.setVisible(false);
             serpentCote2.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite2 && serpent2.isVisible()){
             serpent2.setVisible(false);
             aigle2.setVisible(true);
             aigleCote2.setVisible(false);
             dauphinCote2.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche2 && vide2.isVisible()){
@@ -264,11 +288,13 @@ public class TrapezeController implements Initializable {
             dauphin2.setVisible(true);
             serpentCote2.setVisible(false);
             videCote2.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite2 && vide2.isVisible()){
             vide2.setVisible(false);
             serpent2.setVisible(true);
             serpentCote2.setVisible(false);
             aigleCote2.setVisible(true);
+            sonPierre();
         }
 
         else if (actionEvent.getSource() == flecheGauche2 && dauphin2.isVisible()){
@@ -276,11 +302,13 @@ public class TrapezeController implements Initializable {
             aigle2.setVisible(true);
             videCote2.setVisible(false);
             dauphinCote2.setVisible(true);
+            sonPierre();
         }else if (actionEvent.getSource() == flecheDroite2 && dauphin2.isVisible()){
             dauphin2.setVisible(false);
             vide2.setVisible(true);
             videCote2.setVisible(false);
             serpentCote2.setVisible(true);
+            sonPierre();
         }
 
     }
