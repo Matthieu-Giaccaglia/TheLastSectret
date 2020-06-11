444adecf0492d35ebe00005f0708fb4d081b903d
Maxime <maxime.poujol01@etu.umontpellier.fr>
2020-06-11 16:16:38 +0200

    timer

--- src/main/java/fr/umontpellier/iut/groupe1/thread/ThreadTimer.java
+++ src/main/java/fr/umontpellier/iut/groupe1/thread/ThreadTimer.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Label;
 
 public class ThreadTimer extends Thread implements Runnable{
 
-    private int secondes = 20, minutes = 5;
+    private int secondes = 130;
     private final Label label;
     private boolean running, stopped;
 
@@ -25,7 +25,7 @@ public class ThreadTimer extends Thread implements Runnable{
     public synchronized void run() {
         super.run();
 
-        while (!stopped && minutes > -1){
+        while (!stopped && secondes > 0){
             try {
                 Thread.sleep(10);
             } catch (InterruptedException e) {
@@ -33,25 +33,24 @@ public class ThreadTimer extends Thread implements Runnable{
             }
 
             if(running){
-                if(secondes<10 && secondes > 0){
-                    Platform.runLater(() -> label.setText(minutes + ":0" + secondes));
+                if(secondes%60 <10 && secondes%60 > 0){
+                    Platform.runLater(() -> label.setText(secondes/60 + ":0" + secondes%60));
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
                 }else if(secondes == 0 ) {
-                    Platform.runLater(() -> label.setText(minutes + ":0" + secondes));
+                    Platform.runLater(() -> label.setText(secondes/60 + ":0" + secondes%60));
 
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
-                    minutes--;
-                    secondes = 60;
+
                 }else {
-                    Platform.runLater(() -> label.setText(minutes + ":" + secondes));
+                    Platform.runLater(() -> label.setText(secondes/60 + ":" + secondes%60));
                     try {
                         Thread.sleep(1000);
                     } catch (InterruptedException e) {
