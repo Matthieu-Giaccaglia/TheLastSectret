67c569e355ae2ae6bf3de55e1b600a6e35943d79
Loan <Kalinololo@gmail.com>
2020-06-12 00:00:42 +0200

    GameOver

--- src/main/java/fr/umontpellier/iut/groupe1/controllers/GameOverController.java
+++ src/main/java/fr/umontpellier/iut/groupe1/controllers/GameOverController.java
@@ -1,4 +1,22 @@
 package fr.umontpellier.iut.groupe1.controllers;
 
-public class GameOverController {
+import fr.umontpellier.iut.groupe1.Main;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameOverController implements Initializable {
+
+    @FXML
+    private ImageView background;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        background.fitWidthProperty().bind(Main.stage.widthProperty());
+        background.fitHeightProperty().bind(Main.stage.heightProperty());
+    }
 }
